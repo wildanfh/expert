b5e47955bbb894750ebdf233c054845252370593
wildan <wildanf2005@gmail.com>
2022-10-31 08:52:22 +0700

    film app init

---
+++ lib/presentation/widgets/custom_drawer.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatefulWidget {
+  final Widget content;
+
+  const CustomDrawer({required this.content});
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 250),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: toggle,
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          double slide = 255.0 * _animationController.value;
+          double scale = 1 - (_animationController.value * 0.3);
+
+          return Stack(
+            children: [
+              _buildDrawer(),
+              Transform(
+                transform: Matrix4.identity()
+                  ..translate(slide)
+                  ..scale(scale),
+                alignment: Alignment.centerLeft,
+                child: widget.content,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  void toggle() => _animationController.isDismissed
+      ? _animationController.forward()
+      : _animationController.reverse();
+
+  Widget _buildDrawer() {
+    return Container(
+      child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://raw.githubusercontent.com/dicodingacademy/assets/main/flutter_expert_academy/dicoding-icon.png'),
+            ),
+            accountName: Text('Ditonton'),
+            accountEmail: Text('ditonton@mail.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.movie),
+            title: Text('Movies'),
+          ),
+          ListTile(
+            leading: Icon(Icons.save_alt),
+            title: Text('Watchlist'),
+          ),
+        ],
+      ),
+    );
+  }
+}
